f1affcc23858f4b0d81b787a483d500fd831a010
Eckhart Koeppen <eck@40hz.org>
2018-07-16 14:14:12 +0300

    Mon Jul 16 14:14:12 EEST 2018

--- moter_modem.ml
+++ moter_modem.ml
@@ -134,7 +134,8 @@ let modem _ device =
 let test_modem _ _ =
   Logs.debug (fun m -> m "Starting using test data");
   let heartbeat_data = "\x10\x02\xD8\x41\x82\x44Node\x01\x03" in
-  let test_data = heartbeat_data in
+  let sensor_data = "\x10\x02\xC6\x41\x82\x44Node\xC7\xC4\x82\x21\x0D\xC8\xC4\x82\x21\x18\xFA" in
+  let test_data = sensor_data in
   let ic = Lwt_io.of_bytes ~mode:Lwt_io.input (Lwt_bytes.of_string test_data) in
   let oc = Lwt_io.of_bytes ~mode:Lwt_io.output (Lwt_bytes.create 80) in
   modem_loop ic oc
