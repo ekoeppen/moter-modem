0231a43c7122917406ead870a88faf6b4b94efec
Eckhart Koeppen <eck@40hz.org>
2018-07-20 19:14:53 +0300

    Fri Jul 20 19:14:53 EEST 2018

--- moter_modem.ml
+++ moter_modem.ml
@@ -97,51 +97,51 @@ let handle_sensor_reading s =
   let%map values, _s = sensor_values_of_string n s [] in
   report_values node values
 
-let handle_message s tag =
+let handle_message s tag client =
   let t = tag_of_int tag in
   Logs.info (fun m -> m "Tag: %s" (string_of_tag t));
-  let result = begin
-    match t with
+  let _ = match t with
     | Sensor_reading_tag -> handle_sensor_reading s |> to_success
     | Test_packet_tag -> handle_test_packet s |> to_success
     | Heartbeat_tag -> handle_heartbeat s |> to_success
     | Log_message_tag -> handle_log_message s |> to_success
-    | _ -> Logs.err (fun m -> m "Not a valid start tag %d" tag); true
-  end in
-  if result = false
-  then Logs.err (fun m -> m "Failed to decode message");
-  true
+    | _ -> Logs.err (fun m -> m "Not a valid start tag %d" tag); false
+  in
+  Mqtt.pub "test" "123" client
 
-let handle_packet s =
+let handle_packet s client =
   match Cbor.tag_of_string s with
   | Some (tag, s) ->
-      Lwt.return (handle_message s tag)
+      handle_message s tag client
   | None ->
       (Logs.info (fun m -> m "Message does not start with tag");
-      Lwt.return true)
+      Lwt.return ())
 
-let rec modem_loop ic oc =
+let rec modem_loop ic oc client =
   let packet = Buffer.create 80 in
   let%lwt () = Serial.wait_for_packet ic in
   let%lwt () = Serial.read_packet ic packet in
-  let%lwt ok = handle_packet (Buffer.contents packet) in
-  if ok
-  then modem_loop ic oc
-  else Lwt.return false
+  let%lwt _ok = handle_packet (Buffer.contents packet) client in
+  modem_loop ic oc client
 
-let modem _ device =
+let modem _logging device broker port =
   Logs.debug (fun m -> m "Starting");
   let ic, oc = Serial.open_device device in
-  modem_loop ic oc
+  let%lwt client = Mqtt.start_client ~broker ~port ~ca_file:"" ~cert_file:"" ~key_file:"" in
+  modem_loop ic oc client
 
-let test_modem _ _ =
+let test_modem _logging _device broker port =
   Logs.info (fun m -> m "Starting using test data");
   let _heartbeat_data = "\x10\x02\xD8\x41\x82\x44Node\x01\x03" in
   let sensor_data = "\x10\x02\xC6\x83\x44Node\xC7\xC4\x82\x20\x0D\xC8\xC4\x82\x20\x18\xFA\x03" in
   let test_data = sensor_data in
   let ic = Lwt_io.of_bytes ~mode:Lwt_io.input (Lwt_bytes.of_string test_data) in
   let oc = Lwt_io.of_bytes ~mode:Lwt_io.output (Lwt_bytes.create 80) in
-  modem_loop ic oc
+  let%lwt client = Mqtt.start_client ~broker ~port ~ca_file:"" ~cert_file:"" ~key_file:"" in
+  modem_loop ic oc client
+
+let lwt_wrapper logging device broker port =
+  Lwt_main.run (test_modem logging device broker port)
 
 let setup_log style_renderer level =
   Fmt_tty.setup_std_outputs ?style_renderer ();
@@ -149,18 +149,26 @@ let setup_log style_renderer level =
   Logs.set_reporter (Logs_fmt.reporter ());
   ()
 
-let device_arg =
-  let doc = "Device" in
-  Arg.(required & pos 0 (some string) None & info [] ~docv:"DEVICE" ~doc)
-
 let logging_arg =
   let env = Arg.env_var "MOTER_MODEM_VERBOSITY" in
   Term.(const setup_log $ Fmt_cli.style_renderer () $ Logs_cli.level ~env ())
 
+let device_arg =
+  let doc = "Device" in
+  Arg.(value & opt string "/dev/ttyUSB0" & info ["d"; "device"] ~doc)
+
+let broker_arg =
+  let doc = "MQTT broker" in
+  Arg.(value & opt string "localhost" & info ["h"; "host"] ~doc)
+
+let port_arg =
+  let doc = "Port number" in
+  Arg.(value & opt int 1883 & info ["p"; "port"] ~doc)
+
 let cmd =
   let doc = "MoteR Modem" in
   let exits = Term.default_exits in
-  Term.(const test_modem $ logging_arg $ device_arg),
+  Term.(const lwt_wrapper $ logging_arg $ device_arg $ broker_arg $ port_arg),
   Term.info "moter-modem" ~doc ~exits
 
 let () = Term.(eval cmd |> exit)
